80ac7d4da89e91fbd735ab1b886e5f99cf767855
Rui Lebre <ruilebre@ua.pt>
2020-09-24 08:41:25 +0000

    Trying new configs on shards

--- split-config/host-0/scripts/dicoogle-mongo-configserver.init.js
+++ split-config/host-0/scripts/dicoogle-mongo-configserver.init.js
@@ -4,9 +4,9 @@ rs.initiate(
       configsvr: true,
       version: 1,
       members: [
-         { _id: 0, host : "config01:27017" },
-         { _id: 1, host : "config02:27017" },
-         { _id: 2, host : "config03:27017" }
+         { _id: 0, host: "config01:27017" },
+         { _id: 1, host: "config02:27017" },
+         { _id: 2, host: "config03:27017" }
       ]
    }
 )
